1b63f740b686e075f7451c0e213821fda1173c2b
Ahmet Şeker <aseker@tch.com.tr>
2016-06-06 11:04:44 +0300

    Change assembly title

--- Trigger.Generator/Properties/AssemblyInfo.cs
+++ Trigger.Generator/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Trigger.Generator.V2")]
+[assembly: AssemblyTitle("Trigger.Generator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Trigger.Generator.V2")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyProduct("Trigger.Generator")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
